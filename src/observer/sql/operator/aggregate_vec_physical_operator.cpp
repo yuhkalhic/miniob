4139b5310e32d81e805264377417c6a3c049baab
yuhkalhic <146904269+yuhkalhic@users.noreply.github.com>
2024-11-30 20:20:05 +0800

    Update aggregate_vec_physical_operator.cpp

--- src/observer/sql/operator/aggregate_vec_physical_operator.cpp
+++ src/observer/sql/operator/aggregate_vec_physical_operator.cpp
@@ -8,104 +8,142 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
+#include "sql/operator/aggregate_physical_operator.h"
 #include "common/log/log.h"
-#include "common/lang/ranges.h"
-#include "sql/operator/aggregate_vec_physical_operator.h"
-#include "sql/expr/aggregate_state.h"
-#include "sql/expr/expression_tuple.h"
-#include "sql/expr/composite_tuple.h"
-
-using namespace common;
-
-AggregateVecPhysicalOperator::AggregateVecPhysicalOperator(vector<Expression *> &&expressions)
+#include "storage/table/table.h"
+#include "storage/trx/trx.h"
+RC AggregatePhysicalOperator::open(Trx *trx)
 {
-  aggregate_expressions_ = std::move(expressions);
-  value_expressions_.reserve(aggregate_expressions_.size());
-
-  ranges::for_each(aggregate_expressions_, [this](Expression *expr) {
-    auto *      aggregate_expr = static_cast<AggregateExpr *>(expr);
-    Expression *child_expr     = aggregate_expr->child().get();
-    ASSERT(child_expr != nullptr, "aggregation expression must have a child expression");
-    value_expressions_.emplace_back(child_expr);
-  });
-
-  for (size_t i = 0; i < aggregate_expressions_.size(); i++) {
-    auto &expr = aggregate_expressions_[i];
-    ASSERT(expr->type() == ExprType::AGGREGATION, "expected an aggregation expression");
-    auto *aggregate_expr = static_cast<AggregateExpr *>(expr);
-
-    if (aggregate_expr->aggregate_type() == AggregateExpr::Type::SUM) {
-      if (aggregate_expr->value_type() == AttrType::INTS) {
-        void *aggr_value                     = malloc(sizeof(SumState<int>));
-        ((SumState<int> *)aggr_value)->value = 0;
-        aggr_values_.insert(aggr_value);
-        output_chunk_.add_column(make_unique<Column>(AttrType::INTS, sizeof(int)), i);
-      } else if (aggregate_expr->value_type() == AttrType::FLOATS) {
-        void *aggr_value                       = malloc(sizeof(SumState<float>));
-        ((SumState<float> *)aggr_value)->value = 0;
-        aggr_values_.insert(aggr_value);
-        output_chunk_.add_column(make_unique<Column>(AttrType::FLOATS, sizeof(float)), i);
-      }
-    } else {
-      ASSERT(false, "not supported aggregation type");
-    }
+  if (children_.empty()) {
+    return RC::SUCCESS;
   }
-}
-
-RC AggregateVecPhysicalOperator::open(Trx *trx)
-{
-  ASSERT(children_.size() == 1, "group by operator only support one child, but got %d", children_.size());
-
-  PhysicalOperator &child = *children_[0];
-  RC                rc    = child.open(trx);
-  if (OB_FAIL(rc)) {
-    LOG_INFO("failed to open child operator. rc=%s", strrc(rc));
+  std::unique_ptr<PhysicalOperator> &child = children_[0];
+  RC                                 rc    = child->open(trx);
+  if (rc != RC::SUCCESS) {
+    LOG_WARN("failed to open child operator: %s", strrc(rc));
     return rc;
   }
-
-  while (OB_SUCC(rc = child.next(chunk_))) {
-    for (size_t aggr_idx = 0; aggr_idx < aggregate_expressions_.size(); aggr_idx++) {
-      Column column;
-      value_expressions_[aggr_idx]->get_column(chunk_, column);
-      ASSERT(aggregate_expressions_[aggr_idx]->type() == ExprType::AGGREGATION, "expect aggregate expression");
-      auto *aggregate_expr = static_cast<AggregateExpr *>(aggregate_expressions_[aggr_idx]);
-      if (aggregate_expr->aggregate_type() == AggregateExpr::Type::SUM) {
-        if (aggregate_expr->value_type() == AttrType::INTS) {
-          update_aggregate_state<SumState<int>, int>(aggr_values_.at(aggr_idx), column);
-        } else if (aggregate_expr->value_type() == AttrType::FLOATS) {
-          update_aggregate_state<SumState<float>, float>(aggr_values_.at(aggr_idx), column);
-        } else {
-          ASSERT(false, "not supported value type");
-        }
-      } else {
-        ASSERT(false, "not supported aggregation type");
-      }
-    }
-  }
-
-  if (rc == RC::RECORD_EOF) {
-    rc = RC::SUCCESS;
-  }
-
-  return rc;
-}
-template <class STATE, typename T>
-void AggregateVecPhysicalOperator::update_aggregate_state(void *state, const Column &column)
-{
-  STATE *state_ptr = reinterpret_cast<STATE *>(state);
-  T *    data      = (T *)column.data();
-  state_ptr->update(data, column.count());
-}
-
-RC AggregateVecPhysicalOperator::next(Chunk &chunk)
-{
-  // your code here
-  exit(-1);
-}
-
-RC AggregateVecPhysicalOperator::close()
-{
-  children_[0]->close();
-  LOG_INFO("close group by operator");
   return RC::SUCCESS;
 }
+RC AggregatePhysicalOperator::next()
+{
+    // already aggregated
+    if (result_tuple_.cell_num() > 0){
+        return RC::RECORD_EOF;
+    }
+    RC rc = RC::SUCCESS;
+    PhysicalOperator *oper = children_[0].get();
+    
+    std::vector<Value> result_cells;
+    int tuple_num = 0;//tuple数量
+    while (RC::SUCCESS == (rc = oper->next())) {
+        //get tuple
+        Tuple *tuple = oper->current_tuple();
+        tuple_num++;
+        //do aggregate
+        for (int cell_idx = 0; cell_idx < (int)aggregations_.size(); cell_idx++) {
+            const AggrOp aggregation = aggregations_[cell_idx];
+            
+            Value cell;
+            AttrType attr_type = AttrType::INTS;
+            switch (aggregation){
+            //sum
+            case AggrOp::AGGR_SUM:
+                rc = tuple->cell_at(cell_idx, cell);
+                attr_type = cell.attr_type();
+                if(attr_type == AttrType::INTS or attr_type == AttrType::FLOATS) {
+                  if(static_cast<int>(result_cells.size())!=(int)aggregations_.size()){
+                    result_cells.push_back(cell);
+                  }else{
+                    result_cells[cell_idx].set_float(result_cells[cell_idx].get_float() + cell.get_float());
+                  }
+                }
+                break;
+            //avg
+            case AggrOp::AGGR_AVG:
+                rc = tuple->cell_at(cell_idx, cell);
+                attr_type = cell.attr_type();
+                if(attr_type == AttrType::INTS or attr_type == AttrType::FLOATS) {
+                  if(static_cast<int>(result_cells.size())!=(int)aggregations_.size()){
+                    result_cells.push_back(cell);
+                  }else{
+                    result_cells[cell_idx].set_float(result_cells[cell_idx].get_float() + cell.get_float());
+                  }
+                }
+                break;
+            //max
+            case AggrOp::AGGR_MAX:
+                rc = tuple->cell_at(cell_idx, cell);
+                if(static_cast<int>(result_cells.size())!=(int)aggregations_.size()){
+                    result_cells.push_back(cell);
+                }else{
+                  int flag = cell.compare(result_cells[cell_idx]);
+                  if(flag > 0)//cell.value>result_cells[cell_idx].value
+                    result_cells[cell_idx].set_value(cell);
+                  }
+                break;
+            //min
+            case AggrOp::AGGR_MIN:
+                rc = tuple->cell_at(cell_idx, cell);
+                if(static_cast<int>(result_cells.size())!=(int)aggregations_.size()){
+                    result_cells.push_back(cell);
+                }else{
+                  int flag = cell.compare(result_cells[cell_idx]);
+                  if(flag < 0)//cell.value < result_cells[cell_idx].value
+                    result_cells[cell_idx].set_value(cell);
+                  }           
+                break;
+            //count
+            case AggrOp::AGGR_COUNT:
+                rc = tuple->cell_at(cell_idx, cell);
+                if(static_cast<int>(result_cells.size())!=(int)aggregations_.size()){
+                  result_cells.push_back(cell);
+              }
+                break;
+            case AggrOp::AGGR_COUNT_ALL:
+                rc = tuple->cell_at(cell_idx, cell);
+                if(static_cast<int>(result_cells.size())!=(int)aggregations_.size()){
+                  result_cells.push_back(cell);
+              }
+                break;
+            default:
+                return RC::UNIMPLENMENT;
+            }
+        }
+    }
+    for (int cell_idx = 0; cell_idx < (int)aggregations_.size(); cell_idx++) {
+        const AggrOp aggregation = aggregations_[cell_idx];
+        if(aggregation == AggrOp::AGGR_AVG){
+          float new_float=result_cells[cell_idx].get_float()/tuple_num;
+          result_cells[cell_idx].set_float(new_float);
+        }
+        
+        if(aggregation == AggrOp::AGGR_COUNT || aggregation == AggrOp::AGGR_COUNT_ALL){
+          result_cells[cell_idx].set_float((float)tuple_num);
+        }
+      }   
+    
+    if (rc == RC::RECORD_EOF){
+        rc = RC::SUCCESS;
+    }
+    result_tuple_.set_cells(result_cells);//最终输出的一条元组
+    return rc;
+}
+RC AggregatePhysicalOperator::close()
+{
+  if (!children_.empty()) {
+    children_[0]->close();
+  }
+  return RC::SUCCESS;
+}
+//****************************add_aggregation****************************
+void AggregatePhysicalOperator::add_aggregation(const AggrOp aggregation){
+    aggregations_.push_back(aggregation);
+}
+//***********************************************************************
+//******************************current_tuple****************************
+Tuple *AggregatePhysicalOperator::current_tuple()
+{
+    return &result_tuple_;
+}
+//***********************************************************************
